edc8a60d3a848821ed7a1d0aab9382e336121f1f
olegunvaldsen <gunvalds1@gmail.com>
2022-04-13 16:15:24 +0200

    Added functions for calculating damage, set strenght of axies and an attack function

--- Game engine/battleclass.cpp
+++ Game engine/battleclass.cpp
@@ -2,11 +2,12 @@
 // Created by 47472 on 13.04.2022.
 //
 #include "battleclass.h"
-
+#include <iostream>
+#include <chrono>
+#include <vector>
 
 
 using namespace std::chrono;
-using namespace std;
 
 struct axie{
     unsigned int id = 0;
@@ -17,27 +18,71 @@ struct axie{
     unsigned int speed = 0;
     unsigned int skill = 0;
     int playedCards[2];
+    string strenght = "";
+    bool alive = true;
 
     enum position {front,rear};
     card cards[4];
 };
 
-void damageMultiplyer(axie attacker){
-    double multiplyer = 1;
-    if(attacker.bodypart_1.type == attacker.type){
-        if(attacker.bodypart_2.type == attacker.type){
-
-        }
-        multiplyer *= 0.1;
+void setStrenght(axie a){
+    if(a.type == "Plant"){
+        a.strenght = "Aqua"
+    }else if(a.type == "Aqua"){
+        a.strenght ="Beast"
+    }else if(a.type == "Beast"){
+        a.strenght == "Plant"
     }
 }
 
-void axieAttack(axie attacker, axie defender, int damage){
+void damageCalculator(axie attacker, axie defender){
+    int damage = 100;
 
+    //determines any class advantage/disadvantage based on axie type
+    if(attacker.strenght == defender.type){
+        damage += 15;
+    }else if(defender.strenght == attacker.type){
+        damage -= 15;
+    }else{
+        break;
+    }
+
+    //determines any card advantage based on axie type and card type
+    for(int playedcards : attacker.playedCards) {
+        if (attacker.cards[playedcards].type == attacker.type){
+            damage += 10;
+        }else{
+            break;
+        }
+    }
+    int numberofPlayed = sizeof(attacker.playedcards)/sizeof(attacker.playedcards[0])
+    if(attacker.playedCards >=2){
+        damage += (attacker.skill * 0.55 * numberofPlayed)
+    }
+
+}
+
+void axieAttack(axie attacker, axie defender, int damage){
+    defender.health -= damage;
+    if(defender.health <= 0){
+        axie.alive = false
+    }
 };
 
 int reward(int r){
     return r;
 }
 
+void battle(player p1, player p2){
+    //Establish the players that are battling
+    //determine the order of the axies
+    //input the attacking axie and the front defending axie into damage calculator
+    damageCalculator(p1, p2)
+
+    //attack the defender axie (the front axie of opposing team)
+    axieAttack(a1, a2, damage)
+
+    //repeat until one player is dead.
+
+}
 
