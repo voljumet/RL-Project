39ab3af9033729439e1ac1dee1e012d5ecf47d0a
Peshang <peseng88@gmail.comn>
2022-04-13 10:41:47 +0200

    Added a function for select four random numbers

--- Game engine/main.cpp
+++ Game engine/main.cpp
@@ -203,16 +203,58 @@ player createPlayer(Json::Value &team){
         p.axies[i].morale = team["Axie-0"+to_string(i+1)]["Morale"].asInt();
         p.axies[i].cards[0].type = team["Axie-0"+to_string(i+1)]["Cards"][0].asString();
         p.axies[i].cards[1].type = team["Axie-0"+to_string(i+1)]["Cards"][1].asString();
+        p.axies[i].cards[2].type = team["Axie-0"+to_string(i+1)]["Cards"][0].asString();
+        p.axies[i].cards[3].type = team["Axie-0"+to_string(i+1)]["Cards"][1].asString();
 
         p.axies[i].cards[0].status = card::unusable;
         p.axies[i].cards[1].status = card::unusable;
+        p.axies[i].cards[2].status = card::unusable;
+        p.axies[i].cards[3].status = card::unusable;
     }
 
 
     return p;
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////
+// funtion that select four number between 0 and 7
+vector<int> selectFourNumbers(){
+    vector<int> numbers;
+    int number;
+    for (int i = 0; i < 4; ++i) {
+        number = rand() % 8;
+        while(find(numbers.begin(), numbers.end(), number) != numbers.end()){
+            number = rand() % 8;
+        }
+        numbers.push_back(number);
+    }
+    return numbers;
+}
 
+// function that take four axie objects as parameter and return all cards that are unusable
+vector<card> getUnusableCards(axie axie1, axie axie2, axie axie3, axie axie4){
+    vector<card> unusableCards;
+    for (int i = 0; i < 4; ++i) {
+        if(axie1.cards[i].status == card::unusable){
+            unusableCards.push_back(axie1.cards[i]);
+        }
+    }
+    for (int i = 0; i < 4; ++i) {
+        if(axie2.cards[i].status == card::unusable){
+            unusableCards.push_back(axie2.cards[i]);
+        }
+    }
+    for (int i = 0; i < 4; ++i) {
+        if(axie3.cards[i].status == card::unusable){
+            unusableCards.push_back(axie3.cards[i]);
+        }
+    }
+    for (int i = 0; i < 4; ++i) {
+        if(axie4.cards[i].status == card::unusable){
+            unusableCards.push_back(axie4.cards[i]);
+        }
+    }
+    return unusableCards;
+}
 
 int main() {
     ifstream file("/Users/peshangalo/Documents/Master/First_Year/Second Semester/RL/RL-Project/axie_teams.json");
@@ -220,19 +262,35 @@ int main() {
     Json::Value obj;
     reader.parse(file, obj);
 
+
     int axies [2];
     int i = 3;
     player player1 = createPlayer(obj["Team-01"] );
     player player2 = createPlayer(obj["Team-02"] );
 
     StateController stateController;
-    //stateController.Init(player1, player2);
+    stateController.Init(player1, player2);
+
+
+
+
+    // 8 cards in total, randomly picks four cards, divided between two axies.
+    // 8 cards, 0,1,2,3,4,5,6,7, pick four cards (ex, 0,5,2,3) where (0,1,2,3) is for axie 1 and (4,5,6,7) is for axie 2
+
+    // print the four cards in terminal so that the user can see.
+    // function that select cards based in user inputs. NP: check energy
     string str = "N";
     while (str[0] != 'q')
     {
         stateController.Update();
         cin >> str;
         cout << endl;
+        vector<card> unusableCards = getUnusableCards(player1.axies[0], player1.axies[1], player2.axies[0], player2.axies[1]);
+        // print out unusable cards for each axie with axie name
+        for (int i = 0; i < unusableCards.size(); ++i) {
+            cout << unusableCards[i].type << " ";
+        }
+
       //  if (str[0] == 'a' || str[0] == 'c'){stateController.TransitionTo(str[0], player1, player2);}
     }
     //gameloop();
