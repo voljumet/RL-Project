c2de96c5693e1825a144499d1db7094df41ddd43
Peshang <peseng88@gmail.comn>
2022-04-13 14:51:14 +0200

    added logic for card selection state

--- Game engine/main.cpp
+++ Game engine/main.cpp
@@ -2,15 +2,17 @@
 #include <vector>
 #include <fstream>
 #include <json/json.h>
+#include<time.h>
 using namespace std::chrono;
 using namespace std;
 
 
 struct card{
+    int id ;
     string type = "";
     int damage = 0;
     int defence = 0;
-    enum status {usable, unusable} status;
+    enum status {usable, unusable, used} status;
 };
 
 struct axie{
@@ -56,7 +58,82 @@ vector<int> sortTurnOrder(const player& player1, const player& player2){
 
 
 //////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
+// funtion that select four random numbers between 0 and 7, and a number can not be repeated
+// if the number is repeated, it will be replaced by another number
+// the number of numbers will be 4 and the range is 0 to 7
+// the function will return a vector of 4 numbers
+vector<int> selectFourNumbers(){
+    srand(time(0));
+    vector<int> random_number;
+    int number;
+    int i = 0;
+    while(i < 4){
+        number = rand() % 8;
+        if(find(random_number.begin(), random_number.end(), number) == random_number.end()){
+            random_number.push_back(number);
+            i++;
+        }
+    }
+    return random_number;
+}
+// function that take four axie objects as parameter and return all cards that are unusable
+vector<card> getUnusableCards(axie axie1, axie axie2, axie axie3, axie axie4){
+    vector<card> unusableCards;
+    for (int i = 0; i < 4; ++i) {
+        if(axie1.cards[i].status == card::unusable){
+            unusableCards.push_back(axie1.cards[i]);
+        }
+    }
+    for (int i = 0; i < 4; ++i) {
+        if(axie2.cards[i].status == card::unusable){
+            unusableCards.push_back(axie2.cards[i]);
+        }
+    }
+    for (int i = 0; i < 4; ++i) {
+        if(axie3.cards[i].status == card::unusable){
+            unusableCards.push_back(axie3.cards[i]);
+        }
+    }
+    for (int i = 0; i < 4; ++i) {
+        if(axie4.cards[i].status == card::unusable){
+            unusableCards.push_back(axie4.cards[i]);
+        }
+    }
+    return unusableCards;
+}
 
+vector<int>  showAvailabeCards(player &p)
+{
+    vector<int> usableCards = selectFourNumbers();
+    // check if the number is between 0 and 3, if so find the card that have the same card id
+
+    for (int i = 0; i < 4; ++i) {
+        if(usableCards[i] >= 0 && usableCards[i] <= 3){
+            p.axies[0].cards[usableCards[i]].status = card::usable;
+            cout << "Player " << p.id << " Axie 1 card " << usableCards[i] << " is " << p.axies[0].cards[usableCards[i]].type << endl;
+        }
+        else if(usableCards[i] >= 4 && usableCards[i] <= 7){
+            p.axies[1].cards[usableCards[i]].status = card::usable;
+            cout << "Player " << p.id << " Axie 2 card " << usableCards[i] << " is " << p.axies[1].cards[usableCards[i] - 4].type << endl;
+        }
+    }
+    return usableCards;
+}
+
+void showUsedCards(player &p)
+{
+    //find cards that are used and show them for echa axie in the player
+    for (int i = 0; i < 4; ++i) {
+        if(p.axies[0].cards[i].status == card::used){
+            cout << "Player " << p.id << " Axie 1 card " << p.axies[0].cards[i].id << " is " << p.axies[0].cards[i].type<<" Chosen" << endl;
+        }
+        if(p.axies[1].cards[i].status == card::used){
+            cout << "Player " << p.id << " Axie 2 card " << p.axies[1].cards[i].id << " is " << p.axies[1].cards[i].type<<" Chosen" << endl;
+        }
+    }
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class State
 {
@@ -75,21 +152,80 @@ public:
 
     void SelectCards(player &p1, player &p2)
     {
+
+        vector<int> randomSelectCards =  showAvailabeCards(p1);
         int input;
         cout << "Choose cards for player 1 !" << endl;
-        cin >>input;
-        if (input == 1) {p1.axies->cards[0].status = card::usable;}
-        else if (input == 2) {p1.axies->cards[1].status = card::usable;}
-        else if (input == 3) {p1.axies->cards[2].status = card::usable;}
-        else if (input == 4) {p1.axies->cards[3].status = card::usable;}
-        PrintPlayer(p1,p2);
-    }
-    void PrintPlayer(player &p1, player &p2)
-    {
-        cout << "Player1, card 1: "<< p1.axies->cards[0].status << endl;
-        cout << "Player1: card 2: "<< p1.axies->cards[1].status<< endl;
-        cout << "Player1, card 3: "<< p1.axies->cards[2].status << endl;
-        cout << "Player1: card 4: "<< p1.axies->cards[3].status<< endl;
+        for (int i = 0; i < p1.energy; ++i) {
+            cin >>input;
+            if (input == 1){
+                if (randomSelectCards[0]<= 3){
+                    p1.axies[0].cards[randomSelectCards[0]].status = card::used;
+                }
+                else if (randomSelectCards[0]>= 4 && randomSelectCards[0]<= 7){
+                    p1.axies[1].cards[randomSelectCards[0] - 4].status = card::used;
+                }
+            } else if (input == 2){
+                if (randomSelectCards[1]<= 3){
+                    p1.axies[0].cards[randomSelectCards[1]].status = card::used;
+                }
+                else if (randomSelectCards[1]>= 4 && randomSelectCards[1]<= 7){
+                    p1.axies[1].cards[randomSelectCards[1] - 4].status = card::used;
+                }
+            } else if (input == 3){
+                if (randomSelectCards[2]<= 3){
+                    p1.axies[0].cards[randomSelectCards[2]].status = card::used;
+                }
+                else if (randomSelectCards[2]>= 4 && randomSelectCards[2]<= 7){
+                    p1.axies[1].cards[randomSelectCards[2] - 4].status = card::used;
+                }
+            } else if (input == 4){
+                if (randomSelectCards[3]<= 3){
+                    p1.axies[0].cards[randomSelectCards[3]].status = card::used;
+                }
+                else if (randomSelectCards[3]>= 4 && randomSelectCards[3]<= 7){
+                    p1.axies[1].cards[randomSelectCards[3] - 4].status = card::used;
+                }
+            }
+        }
+
+        showUsedCards(p1);
+        vector<int> randomSelectCards2 =  showAvailabeCards(p2);;
+        int input2;
+        cout << "Choose cards for player 2 !" << endl;
+        for (int i = 0; i < p2.energy; ++i) {
+            cin >>input2;
+            if (input2 == 1){
+                if (randomSelectCards2[0]<= 3){
+                    p2.axies[0].cards[randomSelectCards2[0]].status = card::used;
+                }
+                else if (randomSelectCards2[0]>= 4 && randomSelectCards2[0]<= 7){
+                    p2.axies[1].cards[randomSelectCards2[0] - 4].status = card::used;
+                }
+            } else if (input2 == 2){
+                if (randomSelectCards2[1]<= 3){
+                    p2.axies[0].cards[randomSelectCards2[1]].status = card::used;
+                }
+                else if (randomSelectCards2[1]>= 4 && randomSelectCards2[1]<= 7){
+                    p1.axies[1].cards[randomSelectCards2[1] - 4].status = card::used;
+                }
+            } else if (input2 == 3){
+                if (randomSelectCards2[2]<= 3){
+                    p2.axies[0].cards[randomSelectCards2[2]].status = card::used;
+                }
+                else if (randomSelectCards2[2]>= 4 && randomSelectCards2[2]<= 7){
+                    p2.axies[1].cards[randomSelectCards2[2] - 4].status = card::used;
+                }
+            } else if (input2 == 4){
+                if (randomSelectCards2[3]<= 3){
+                    p2.axies[0].cards[randomSelectCards2[3]].status = card::used;
+                }
+                else if (randomSelectCards2[3]>= 4 && randomSelectCards2[3]<= 7){
+                    p2.axies[1].cards[randomSelectCards2[3] - 4].status = card::used;
+                }
+            }
+        }
+        showUsedCards(p2);
     }
     // Constructor
     Card_Selection_State(player &p1, player &p2)
@@ -102,6 +238,7 @@ public:
         cout << "Card_Selection_State is discreated!" << endl;
     };
 };
+
 
 class Attack_State: public State
 {
@@ -164,6 +301,8 @@ player createPlayer(Json::Value &team){
     player p;
 
     for (int i = 0; i < 2 ; ++i) {
+        p.id = i;
+        p.energy = 2;
         string axie = "Axie-0";
         axie += to_string(i);
         p.id = team["Team-id"].asInt();
@@ -182,92 +321,47 @@ player createPlayer(Json::Value &team){
         p.axies[i].cards[1].status = card::unusable;
         p.axies[i].cards[2].status = card::unusable;
         p.axies[i].cards[3].status = card::unusable;
+
+        if ( i == 0){
+
+        p.axies[i].cards[0].id = 0;
+        p.axies[i].cards[1].id = 1;
+        p.axies[i].cards[2].id = 2;
+        p.axies[i].cards[3].id = 3;
+        }
+        else{
+        p.axies[i].cards[0].id = 4;
+        p.axies[i].cards[1].id = 5;
+        p.axies[i].cards[2].id = 6;
+        p.axies[i].cards[3].id = 7;
+        }
     }
 
 
     return p;
 }
-/////////////////////////////////////////////////////////////////////////////////////////////////
-// funtion that select four number between 0 and 7
-vector<int> selectFourNumbers(){
-    vector<int> numbers;
-    int number;
-    for (int i = 0; i < 4; ++i) {
-        number = rand() % 8;
-        while(find(numbers.begin(), numbers.end(), number) != numbers.end()){
-            number = rand() % 8;
-        }
-        numbers.push_back(number);
-    }
-    return numbers;
-}
 
-// function that take four axie objects as parameter and return all cards that are unusable
-vector<card> getUnusableCards(axie axie1, axie axie2, axie axie3, axie axie4){
-    vector<card> unusableCards;
-    for (int i = 0; i < 4; ++i) {
-        if(axie1.cards[i].status == card::unusable){
-            unusableCards.push_back(axie1.cards[i]);
-        }
-    }
-    for (int i = 0; i < 4; ++i) {
-        if(axie2.cards[i].status == card::unusable){
-            unusableCards.push_back(axie2.cards[i]);
-        }
-    }
-    for (int i = 0; i < 4; ++i) {
-        if(axie3.cards[i].status == card::unusable){
-            unusableCards.push_back(axie3.cards[i]);
-        }
-    }
-    for (int i = 0; i < 4; ++i) {
-        if(axie4.cards[i].status == card::unusable){
-            unusableCards.push_back(axie4.cards[i]);
-        }
-    }
-    return unusableCards;
-}
+// create a function that check if all cards status for each axie is used if true, then change the status to unusable
 
-void gameloop(){
-
-
-
-    int choose_card;
-
-    cin >> choose_card;
-    switch (choose_card) {
-        case 1:
-            cout << "You chose 1" << endl;
-            break;
-        case 2:
-            cout << "You chose 2" << endl;
-            break;
-        case 3:
-            cout << "You chose 3" << endl;
-            break;
-        default:
-            cout << "You chose something else" << endl;
-            break;
-    }
-}
 
 int main() {
-   // gameloop();
-    // 8 cards in total, randomly picks four cards, divided between two axies.
-    // 8 cards, 0,1,2,3,4,5,6,7, pick four cards (ex, 0,5,2,3) where (0,1,2,3) is for axie 1 and (4,5,6,7) is for axie 2
-
-    // print the four cards in terminal so that the user can see.
-    // function that select cards based in user inputs. NP: check energy
-
     int axies [2];
     ifstream file("/Users/peshangalo/Documents/Master/First_Year/Second Semester/RL/RL-Project/axie_teams.json");
     Json::Reader reader;
     Json::Value obj;
     reader.parse(file, obj);
 
+    // ask user to choose axie team
+    string axie_team;
+    cout << "Choose your axie team ()" << endl;
+    cin >> axie_team;
+    // ask user to choose another axie team
+    string axie_team2;
+    cout << "Choose your second axie team" << endl;
+    cin >> axie_team2;
 
-    player player1 = createPlayer(obj["Team-01"] );
-    player player2 = createPlayer(obj["Team-02"] );
+    player player1 = createPlayer(obj["Team-" +axie_team] );
+    player player2 = createPlayer(obj["Team-" +axie_team2] );
 
     StateController stateController;
     stateController.Init(player1, player2);
