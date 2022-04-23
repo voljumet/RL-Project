fd052ac5f7ce1b6f4913b0253bce6b535717be49
voljumet <alexander.laursen@gmail.com>
2022-04-23 03:09:58 +0200

    binding func returns a bit-array ready for DQL

--- Game engine/conversion.cpp
+++ Game engine/conversion.cpp
@@ -7,7 +7,7 @@ using namespace std;
 class conv {
 public:
 
-    int conv_type(string type){
+    int convert_str_to_int(string type){
         //create cases for the types plant, aqua and beast and return int 0, 1 or 2
         if(type == "plant"){
             return 0;
@@ -52,29 +52,28 @@ public:
 
     // create a function that converts a card into a vector of ints
     vector<int> card_conv(Main::card &card){
-        vector<int> returnVector;
+        vector<int> returnVector1, returnVector2;
 
-        returnVector.push_back(card.card_status);
-        returnVector.push_back(conv_type(card.type));
-        returnVector.push_back(card.damage);
-        returnVector.push_back(card.defence);
+        returnVector1 = convert_to_bits(card.card_status, 2);
+        returnVector2 = merge_vectors(returnVector1, convert_to_bits(convert_str_to_int(card.type),2));
+        returnVector1 = merge_vectors(returnVector2, convert_to_bits(card.damage,8));
+        returnVector2 = merge_vectors(returnVector1, convert_to_bits(card.defence,7));
 
-        return returnVector;
+        return returnVector2;
     }
 
     vector<int> axie_conv(Main::axie &axie){
         vector<int> returnVector1, returnVector2;
 
-//        returnVector.push_back(axie.strenght); /// does not work, it will be based on card and attacker
         returnVector1 = card_conv(axie.cards[3]);
         returnVector2 = merge_vectors(returnVector1, card_conv(axie.cards[2]));
         returnVector1 = merge_vectors(returnVector2, card_conv(axie.cards[1]));
         returnVector2 = merge_vectors(returnVector1, card_conv(axie.cards[0]));
-        returnVector2.push_back(conv_type(axie.type));
-        returnVector2.push_back(axie.position);
-        returnVector2.push_back(axie.alive);
-        returnVector2.push_back(axie.speed);
-        returnVector2.push_back(axie.health);
+        returnVector1 = merge_vectors(returnVector2, convert_to_bits(convert_str_to_int(axie.type),2));
+        returnVector1.push_back(axie.position);
+        returnVector1.push_back(axie.alive);
+        returnVector2 = merge_vectors(returnVector1, convert_to_bits(axie.speed,6));
+        returnVector1 = merge_vectors(returnVector2, convert_to_bits(axie.health,10));
 
         return returnVector1;
     }
@@ -84,8 +83,8 @@ public:
         Main::axie axie;
         returnVector1 = axie_conv(playa.axies[1]);
         returnVector2 = merge_vectors(returnVector1, axie_conv(playa.axies[0]));
-        returnVector2.push_back(playa.energy);
-        returnVector2.push_back(playa.rank);
+        returnVector1 = merge_vectors(returnVector2, convert_to_bits(playa.energy,4));
+        returnVector2 = merge_vectors(returnVector1, convert_to_bits(playa.rank,12)); /// can achieve rank 4096
 
         return returnVector2;
     }
