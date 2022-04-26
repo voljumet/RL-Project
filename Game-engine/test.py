b26ba1493ed0d379a7992bb3e56ccfcf8e0f6d5f
voljumet <alexander.laursen@gmail.com>
2022-04-26 13:01:37 +0200

    working game inside test.py

--- Game-engine/test.py
+++ Game-engine/test.py
@@ -1,6 +1,7 @@
-# Load the cpp library libjuice.so
+# Load the cpp library DEEPAXIE.so
 import DeepAxie
 import numpy as np
+import random
 
 # Prints working directory, should be in the same directory as this file e.g. inside "Game-engine"
 print(DeepAxie.PrintWorkingDir())
@@ -18,25 +19,41 @@ while True:
 
     # use rand to pick two random numbers from an array.
     # Choose cards among the cards drawn from deck for both players.
-    rand1 = np.random.randint(0,drawCardsFromDeck.__len__())
-    rand2 = np.random.randint(0,drawCardsFromDeck.__len__())
+    rand = []
+
+    if drawCardsFromDeck.__len__() != 1:
+        rand = random.sample(drawCardsFromDeck, 2)    
+    else:
+        rand = drawCardsFromDeck
+        rand.append(0)
     
-    while rand1 == rand2:
-        rand2 = np.random.randint(0,4)
+    # rand1 = np.random.randint(0,drawCardsFromDeck.__len__())
+    # rand2 = np.random.randint(0,drawCardsFromDeck.__len__())
+    
+    # while rand1 == rand2:
+    #     if drawCardsFromDeck.__len__() == 1:
+    #         rand2 = 0
+    #     else:
+    #         rand2 = np.random.randint(0,drawCardsFromDeck.__len__())
 
 
-    print("card1 index chosen: ",rand1)
-    print("card2 index chosen: ",rand2)
-    print("card1 chosen: ",drawCardsFromDeck[ rand1 ])
-    print("card2 chosen: ",drawCardsFromDeck[ rand2 ])
-    gameState.chooseCards(1, [ drawCardsFromDeck[ rand1 ], drawCardsFromDeck[ rand2 ] ])
+    print("card1 index chosen: ",rand)
+    print("card2 chosen: ",drawCardsFromDeck)
+    gameState.chooseCards(1, rand)
 
     # Presenting cards from the deck of cards for players inside the gameState.
     drawCardsFromDeck = gameState.drawCardsFromDeck(2)
+
+    rand = []
+
+    if drawCardsFromDeck.__len__() != 1:
+        rand = random.sample(drawCardsFromDeck, 2)    
+    else:
+        rand = drawCardsFromDeck
+        rand.append(0)
     # use rand to pick two random numbers from an array.
     # Choose cards among the cards drawn from deck for both players.
-    rand = np.random.randint(0,4,2)
-    gameState.chooseCards(2, [drawCardsFromDeck[rand[0]], drawCardsFromDeck[rand[1]]])
+    gameState.chooseCards(2, rand)
 
 
     # Run attackstate for the gameState, returns an int.
@@ -45,8 +62,10 @@ while True:
     # The int returned by attackstate is like the if else statement 
     if state == 1:
         print("Player 1 wins")
+        break
     elif state == 2:
         print("Player 2 wins")
+        break
 
 
     # Returns gameState with a matrix containing players represented as decimal numbers.
