f3420e6014ce29be0faf2bd8294e5d6ae5644162
Vol'jum <35361332+voljumet@users.noreply.github.com>
2022-05-25 14:37:14 +0200

    changes to DQN Hyper parameters graphics added

--- Game-engine/DQN.py
+++ Game-engine/DQN.py
@@ -32,18 +32,22 @@ class DQN:
 
     """ Implementation of deep q learning algorithm """
 
-    def __init__(self, action_space, state_space):
+    def __init__(self, action_space, state_space, load_model):
 
         self.action_space = action_space
         self.state_space = state_space
         self.epsilon = 1
         self.gamma = .98
         self.batch_size = 64
-        self.epsilon_min = .01
-        self.epsilon_decay = .995
+        self.epsilon_min = .1
+        self.epsilon_decay = .999
         self.learning_rate = 0.0001
         self.memory = deque(maxlen=100000)
-        self.model = self.build_model()
+        
+        if load_model:
+            self.model = load_model
+        else:
+            self.model = self.build_model()
 
     def build_model(self):
         #  to dense layer på siden av hverandre
@@ -74,7 +78,7 @@ class DQN:
 
         if len(self.memory) < self.batch_size:
             return
-
+        # print("size mem: ", len(self.memory))
         minibatch = random.sample(self.memory, self.batch_size)
         states = np.array([i[0] for i in minibatch])
         actions = np.array([i[1] for i in minibatch])
@@ -88,6 +92,7 @@ class DQN:
         targets = rewards + self.gamma*(np.amax(self.model.predict_on_batch(next_states), axis=1))*(1-dones)
         targets_full = self.model.predict_on_batch(states)
 
+
         ind = np.array([i for i in range(self.batch_size)])
         targets_full[[ind], [actions]] = targets
 
@@ -95,126 +100,129 @@ class DQN:
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
 
+    def saver(self, episode, ratio1, returnrewards1, returnrewards2, tot_rounds):
+        df = pd.DataFrame({"episode" : np.arange(episode), "win-loss ratio" : ratio1, "reward p1" : returnrewards1, "reward p2" : returnrewards2, "rounds" : tot_rounds})
+        df.to_csv("log/"+time +"/numbers.csv", index=False)
+        self.model.save("log/"+time +"/model.h5")
+        # print("file saved!")
+
 def post_process_state(state, state_space):
-    return np.reshape(state, (1, state_space))/1200
+    return np.reshape(state, (1, state_space))#/1200
 
-def train_dqn(episode):
 
-    returnrewards = []
-    ratio = []
+def train_dqn(episode, load_model):
+
+    returnrewards1 = []
+    returnrewards2 = []
+    ratio1 = []
+    ratio2 = []
     tot_rounds = []
 
     action_space = 88
     state_space = 88
-    max_steps = 100
+    max_steps = 1000
     
     p1 = 0
+    p2 = 0
     calc = 0
-    selected_player = "player_1"
-    agent = DQN(action_space, state_space)
+    # TODO sjekk om selected player ødelegger alt før det ble fikset!
+    # selected_player = "player_1"
+    agent = DQN(action_space, state_space, load_model)
     for e in range(episode):
         states = env.reset()
-        states = {k: post_process_state(v, state_space) for k, v in states.items()}
-        score = 0
+        inni = states['player_0'].__len__()
+        states2 = {k: post_process_state(v, state_space) for k, v in states.items()}
+        inn2 = states2['player_0'].__len__()
+        score1 = 0
+        score2 = 0
         for rounds in range(max_steps):
-
             actions = dict(
-                #player_0 = random.randint(0,8),
-                player_0 = agent.act(states["player_0"]),
-                # player_1 = random.randint(0,8)
-                player_1 = agent.act(states["player_1"])
+                #player_0 = random.randint(0,81),
+                player_0 = agent.act(states2["player_0"]),
+                player_1 = 0
+                # player_1 = agent.act(states["player_1"])
             )
+            # pick two random numbers between 0 and 8
+
+
             
             next_states, rewards, dones, infos = env.step(actions)
-            next_states = {k: post_process_state(v,state_space) for k, v in next_states.items()}
+            inn3 = next_states['player_0'].__len__()
 
-            score += rewards[selected_player]
+            next_states2 = {k: post_process_state(v,state_space) for k, v in next_states.items()}
+            inn4 = next_states2.__len__()
+
+            # score += rewards[selected_player]
+            score1 += rewards["player_0"]
+            score2 += rewards["player_1"]
             for (agent_name, state), (_, action), (_, reward), (_, next_state), (_, done) in zip(states.items(), actions.items(), rewards.items(), next_states.items(), dones.items()):  
                 agent.remember(state, action, reward, next_state, done)
             states = next_states
             agent.replay()
 
+            # TODO denne må endres til å se hvilken player som vinner, ikke høyest reward?
             if any(dones.values()):
+                agent.saver(e, ratio1, returnrewards1, returnrewards2, tot_rounds)
                 if rewards["player_0"] > rewards["player_1"]:
                     winner = "player_0"
                     p1 += 1
                 else:
                     winner = "player_1"
-                # todo legge til player til info i step function
+                    p2 += 1
+                # TODO legge til player til info i step function
 
-                calc = round((p1+0.0000001)/(e+1), 3)
-                ratio.append(calc)
-                print("episode: {}/{}, rounds: {}, score: {}, winner: {}, win-ratio: {}".format(e, episode, rounds, round(score,3), winner, calc))
+                calc = round((p1+0.0000001)/(p1+p2+0.000001), 3)
+                ratio1.append(calc)
+                # ratio2.append(calc)
+                print("episode: {}/{}, rounds: {}, score p1: {}, score p2: {}, winner: {}, win-ratio: {}".format(e, episode, rounds, round(score1,3), round(score2,3),winner, calc))
                 break
 
 
             if rounds == (max_steps-1):
-                calc = round((p1+0.0000001)/(e+1), 3)
-                ratio.append(calc)
-                print("episode: {}/{}, rounds: {}, score: {}, winner: Draw, win-ratio: {}".format(e, episode, rounds, round(score,3), calc))
+                calc = round((p1+0.0000001)/(p1+p2+0.000001), 3)
+                ratio1.append(calc)
+                # ratio2.append(calc)
+                print("episode: {}/{}, rounds: {}, score p1: {}, score p2: {}, winner: {}, win-ratio: {}".format(e, episode, rounds, round(score1,3), round(score2,3),winner, calc))
                 break
 
-        returnrewards.append(score)
+        returnrewards1.append(score1)
+        returnrewards2.append(score2)
         tot_rounds.append(rounds)
     
-    # save model each 100th episode
-    if e % 10 == 0:
-        torch.save({
-            'episode': e,
-            'returnrewards': returnrewards,
-            'ratio': ratio,
-            'tot_rounds': tot_rounds,
-            'model_state_dict': self.model.state_dict(),
-            'optimizer_state_dict': self.optimizer.state_dict(),
-            'loss': LOSS,
-            }, time +"/training")
-    return returnrewards, ratio, tot_rounds
+    return returnrewards1, returnrewards2, ratio1, tot_rounds
+
+
+
+def plotter(ep, type, data):
+    fig = plt.figure(figsize=(12, 6))
+    plt.plot([i for i in range(ep)], data, label=type)    
+    plt.xlabel('episodes')
+    plt.ylabel(type)
+    fig.savefig("log/"+time +"/"+type+ '.jpg', bbox_inches='tight', dpi=150)
+
 
 print("Let's GOOOO")
 
-
 if __name__ == '__main__':
-    # define data
-    # data = asarray([[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]])
-    # save to csv file
     
-
-    os.chdir('log')
-    os.mkdir(time)
+    # Create folder to log training session
+    os.mkdir("log/"+time)
     
-    # save files to save the configuration
-    shutil.copy('../DQN.py', time+'/DQN.txt')
-    shutil.copy('../game.py', time+'/game.txt')
+    # save files to store the hyperparameters that was used
+    shutil.copy('DQN.py', "log/"+time+'/DQN.txt')
+    shutil.copy('game.py', "log/"+time+'/game.txt')
     
-    os.chdir('..')
+    load_model = False
+    ep = 10000
+    returnrewards1, returnrewards2, ratio, rounds = train_dqn(ep, load_model)
     
-    ep = 1000
-    returnrewards, ratio, rounds = train_dqn(ep)
-    
-    os.chdir('log')
     if ratio.__len__() < ep:
         ratio.append(0)
     
-    fig = plt.figure(figsize=(12, 6))
-    plt.plot([i for i in range(ep)], ratio, label="win-ratio")    
-    plt.xlabel('episodes')
-    plt.ylabel('win-ratio')
-    fig.savefig(time +"/training" +  '.jpg', bbox_inches='tight', dpi=150)
-    # plt.show()
-    
-    fig = plt.figure(figsize=(12, 6))
-    plt.plot([i for i in range(ep)], returnrewards, label="reward")
-    plt.xlabel('episodes')
-    plt.ylabel('reward')
-    fig.savefig(time +"/reward" +  '.jpg', bbox_inches='tight', dpi=150)
-    # plt.show()
-    
-    fig = plt.figure(figsize=(12, 6))
-    plt.plot([i for i in range(ep)], rounds, label="rounds")
-    plt.xlabel('episodes')
-    plt.ylabel('rounds')
-    fig.savefig(time +"/rounds" +  '.jpg', bbox_inches='tight', dpi=150)
-    # plt.show()
+    plotter(ep=ep, type="win-ratio", data=ratio)
+    plotter(ep=ep, type="reward", data=returnrewards1)
+    plotter(ep=ep, type="rounds", data=rounds)
 
-    df = pd.DataFrame({"episode" : np.arange(ep), "win-loss ratio" : ratio, "reward" : returnrewards, "rounds" : rounds})
-    df.to_csv(time +"/numbers.csv", index=False)
+
+
+    
